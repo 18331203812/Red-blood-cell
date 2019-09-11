844b693a0f26bd9300256bfeebc82950fbeebbf7
小名美儿大名俊儿人称帅男 <505548885@qq.com>
2019-09-11 17:01:29 +0800

    9/11

---
+++ component/frame/frame.js
@@ -0,0 +1,26 @@
+// component/frame/frame.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    status:{
+      type:String,
+      value:"NoOneself"
+    }
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
