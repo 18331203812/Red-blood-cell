3cd1b96018818d33bca266209ed1cecbc19715bc
小名美儿大名俊儿人称帅男 <505548885@qq.com>
2019-09-12 17:43:27 +0800

    9/12

---
+++ component/loadmore/loadmore.js
@@ -0,0 +1,27 @@
+// component/loadmore/loadmore.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  externalClasses: ['page', "page__bd", "weui-loadmore", "weui-loading", 'weui-loadmore__tips'],
+  properties: {
+    isShow: {
+      type: Boolean,
+      value: true
+    }
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
